5aa5a8f5f590c10d3fbe0df0d633cab613230db2
chanh <maivanchanh321@gmail.com>
2021-07-10 12:51:02 +0700

    namecard

---
+++ index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html>
+	<Header>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>Mai Văn Chánh</title>
+		<link rel="stylesheet" type="text/css" href="../stylesheets/style.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+         
+	</Header>
+
+<body>
+<section >
+    <div>
+       <div class="center1">
+            <div>
+                <img class="anh" src="../images/daidien.jpg" alt="">
+            </div>
+            <div class="text1" style="color:#f0eff8;" >
+                <h3 class=" text1">MAI VĂN CHÁNH</h3>
+                <div class="desc"><span> ---> Chanh Tony <--- </span>
+            </div>
+            </div>
+        </div> 
+        <marquee class="ma">
+        	 Mai Văn Chánh !!! Xin cảm ơn bạn đã ghé thăm thông tin cá nhân của mình (^v^)
+        </marquee>
+            <div class="kt"> 
+
+            </div> 
+            </div> 
+    <div class="buttonBig">                  
+        <div class="buttonlong" style="border: 14px double #512F03">
+            <div class="buttonlong1" style="background-color:#FBD8A9">
+                    <a href="https://zalo.me/0367979432">   
+                    <div class="anh1"><img class="anh2" src="../images/zalo.gif" alt="zalo"></div>
+                        <div class="text"><p>Zalo</p></div>
+                    </a>
+            </div>
+        </div>
+        <div class="kt">
+
+         </div>
+         <div class="buttonlong" style="border: 14px double #464B06">
+            <div class="buttonlong1" style="background-color:#EBF29B">
+                    <a href="https://www.facebook.com/messages/t/100009393901983/openapp"> 
+                    <div class="anh1"><img class="anh2" src="../images/mes.png" alt="messager"></div>
+                        <div class="text"><p>Messanger</p></div>
+                    </a>
+            </div>
+         </div>
+         <div class="kt">
+             
+         </div>
+         <div class="buttonlong" style="border: 14px double #2C5709">
+            <div class="buttonlong1" style="background-color:#B0F17B">
+                    <a href="https://www.tiktok.com/@maivanchanh?lang=vi-VN">  
+                    <div class="anh1"><img class="anh2" src="../images/tiktok.png" alt="tiktok"></div>
+                        <div class="text"><p>TikTok</p></div>
+                    </a>
+            </div>
+         </div>
+         <div class="kt">     
+         </div>                
+        <div class="buttonlong" style="border: 14px double #0A5A42 ">
+            <div class="buttonlong1" style="background-color:#7AFFD8">
+                    <a href="https://www.instagram.com/tonychanh99/">   
+                    <div class="anh1"><img class="anh2" src="../images/instagram.png" alt="instagram"></div>
+                        <div class="text"><p>Instagram</p></div>
+                    </a>
+            </div>
+         </div>
+         <div class="kt">
+
+         </div>
+         <div class="buttonlong" style="border: 14px double #050B52">
+            <div class="buttonlong1" style="background-color:#7882FA">
+                    <a href="https://twitter.com/chanh-tony"> 
+                    <div class="anh1"><img class="anh2" src="../images/twitter.png" alt="twitter"></div>
+                        <div class="text"><p>Twitter</p></div>
+                    </a>
+            </div>
+         </div>
+         <div class="kt">
+             
+         </div>
+         <div class="buttonlong" style="border: 14px double #4F0B52">
+            <div class="buttonlong1" style="background-color:#EEACF2">
+                    <a href="https://github.com/MaiVanChanh?fbclid=IwAR0r-bnqYW1CYY3xDpLFsXugg7zZoy00JKalAKS9-6Oy-C7_dVYKd9a1Wmw">  
+                    <div class="anh1"><img class="anh2" src="../images/githup.png" alt="githup"></div>
+                        <div class="text"><p>GitHup</p></div>
+                    </a>
+            </div>
+         </div>
+         <div class="kt">
+             
+         </div>
+ </div>
+                <div class="kt">
+                </div>
+    <div>
+    <div class="shore" >
+<ul>
+<li>
+    <a href="tel://0961089798" >
+        <img src="../images/phone.gif" alt="phone" class="icon" >
+        <p class="icon2">Phone</p>
+    </a>
+</li>
+<li>
+    <a href="https://wa.me/0961089798">
+        <img src="../images/whatapp.png" alt="whatapp" class="icon" >
+        <p class="icon2">Whatapp</p>
+    </a>
+</li>
+<li>
+    <a href="https://www.facebook.com/ChanhHendSome"  >
+        <img src="../images/facebook.png" alt="facebook" class="icon" >
+        <p class="icon2">FaceBook</p>
+    </a>
+</li>
+<li>
+    <a href="mailto:maivanchanh9999@gmail.com"  >
+        <img src="../images/gmail.png" alt="gmail" class="icon" >
+        <p class="icon2">Gmail</p>
+    </a>
+</li>
+<li>
+    <a href="https://www.youtube.com/channel/UCvz_nShH1wN3jlYgWCMlRrA" >
+        <img src="../images/youtobe.jpg" alt="youtobe" class="icon" >
+        <p class="icon2">Youtobe</p>
+    </a>
+</li>
+<li>
+    <a href="https://maps.apple.com/place?address=%C4%90%C6%B0%E1%BB%9Dng%20S%E1%BB%91%2011%2C%20P.%20Hi%E1%BB%87p%20Th%C3%A0nh%2C%20TP.%20Th%E1%BB%A7%20D%E1%BA%A7u%20M%E1%BB%99t%2C%20B%C3%ACnh%20D%C6%B0%C6%A1ng%2C%20Vi%E1%BB%87t%20Nam&ll=11.000273%2C106.669007&q=V%E1%BB%8B%20tr%C3%AD%20c%E1%BB%A7a%20t%C3%B4i&_ext=EiYpWO78YPH9JUAxiiDZb4aqWkA5UESU0YoCJkBBnlgxXxyrWkBQBA%3D%3D&t=m&fbclid=IwAR0kF44fwU-bEKPzXjugw2tV_FpGSMzhwO7nHelIW636LmPb0ym5bG6XeFU" >
+        <img src="../images/map.gif" alt="ggmap" class="icon" >
+        <p class="icon2">Map</p>
+    </a>
+</li>
+</ul>
+</div>
+    </div>
+<div >
+<a href="https://www.facebook.com/messages/t/100009393901983" >
+<div>
+        <img src="../images/LOGO.png" alt="chanh" class="logo">
+      
+        <p class="ter1">NÂNG TẦM GIÁ TRI</p>
+    </a>
+    </div>   
+    <div>
+
+    </div>   
+    <div class="ter"><p>DESIGN BY M.V.CHÁNH </p></div>
+</div>
+</section>
+</body>
+</html>
